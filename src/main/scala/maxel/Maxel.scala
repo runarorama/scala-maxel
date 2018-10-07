4685bc488de47f840220ffda2192e16aceed6b96
Runar Bjarnason <runarorama@gmail.com>
2018-10-06 22:29:21 -0400

    Generalized maxel

--- src/main/scala/maxel/Maxel.scala
+++ src/main/scala/maxel/Maxel.scala
@@ -1,42 +1,47 @@
 package maxel
 
-import mset._
-import spire.algebra.Eq
-import spire.algebra.Order
-import spire.syntax.all._
-import spire.math._
-import spire.compat._
-import Realm.NaturalRealm
+import  mset._
+import  spire.algebra.{AdditiveMonoid, Eq, MultiplicativeMonoid,
+                       MultiplicativeSemigroup}
+import  spire.algebra.lattice.{JoinSemilattice, MeetSemilattice}
+import  spire.algebra.Order
+import  spire.math._
+import  spire.compat._
+import  spire.syntax.all._
 
 /**
  * Maxels are a generalization of matrices. A matrix can be viewed as an image
- * (a kind of colimit) of a maxel. Matrices and maxels are in an adjunction,
- * where a map from maxels to matrices is a right adjoint.
+ * of a maxel. Matrices and maxels are in an adjunction, where a map from
+ * maxels to matrices is a right adjoint.
  *
- * A maxel is represented by a multiset of pixels.
+ * A maxel is represented by a multiset of pixels. The "location" of the pixel
+ * is in `I`, while the value of the pixels is in `A`.
  */
-case class Maxel[A](rep: MSet[Natural, Pixel[A]]) {
+case class Maxel[I,A](rep: MSet[A, Pixel[I]]) {
+  import MSet._
 
   /** The size of a maxel is its number of pixels. */
-  def size = rep.size
+  def size(implicit M: AdditiveMonoid[A]): A = rep.size
 
   /**
    * The [[frame]] of a maxel is the set of its pixels, also known as its
    * [[support]].
    */
-  def frame: Set[Pixel[A]] = rep.toSet
+  def frame: Set[Pixel[I]] = rep.toSet
 
   /**
    * The [[support]] of a maxel is the set of its pixels, also known as its
    * [[frame]].
    */
-  def support: Set[Pixel[A]] = frame
+  def support: Set[Pixel[I]] = frame
 
   /**
    * The extent of a maxel is the largest row and column of any of its pixels,
    * respectively. The extent of the empty maxel is undefined.
    */
-  def extent(implicit O: Order[A]): Option[Pixel[A]] = {
+  def extent(implicit O: Order[I],
+                      M: AdditiveMonoid[A],
+                      A: Eq[A]): Option[Pixel[I]] = {
     val ps = frame
     if (isEmpty) None else Some(Pixel(ps.maxBy(_.row).row, ps.maxBy(_.col).col))
   }
@@ -45,86 +50,125 @@ case class Maxel[A](rep: MSet[Natural, Pixel[A]]) {
    * The intent of a maxel is the smallest row and column of any of its pixels,
    * respectively. The intent of the empty maxel is undefined.
    */
-  def intent(implicit O: Order[A]): Option[Pixel[A]] = {
+  def intent(implicit O: Order[I],
+                      M: AdditiveMonoid[A],
+                      E: Eq[A]): Option[Pixel[I]] = {
     val ps = frame
     if (isEmpty) None else Some(Pixel(ps.minBy(_.row).row, ps.minBy(_.col).col))
   }
 
   /** A maxel is diagonal when all its pixels are diagonal. */
-  def isDiagonal(implicit E: Eq[A]): Boolean = frame.forall(_.isDiagonal)
+  def isDiagonal(implicit E: Eq[I]): Boolean = frame.forall(_.isDiagonal)
 
   /**
    * The transpose of a maxel is the maxel of transposes of its pixels.
    */
-  def transpose: Maxel[A] = Maxel(rep.map(_.transpose))
+  def transpose(implicit M: MultiplicativeMonoid[A],
+                         N: AdditiveMonoid[A],
+                         E: Eq[A]): Maxel[I,A] =
+    Maxel(rep.map(_.transpose))
 
   /**
    * A maxel is symmetric if it is isomorphic to its transpose.
    */
-  def symmetric(implicit E: Eq[A]): Boolean = transpose === this
+  def symmetric(implicit E: Eq[I], A: Eq[A],
+                         M: MultiplicativeMonoid[A],
+                         N: AdditiveMonoid[A]): Boolean = transpose === this
 
   /**
    * Two maxels are equal when they consist of the same pixels in the same
    * measure.
    */
-  def ===(m: Maxel[A])(implicit E: Eq[A]): Boolean = rep === m.rep
+  def ===(m: Maxel[I,A])(implicit E: Eq[I],
+                                  A: Eq[A],
+                                  M: AdditiveMonoid[A]): Boolean =
+    Eq[MSet[A,Pixel[I]]].eqv(rep, m.rep)
 
   /**
    * A maxel is empty if it contains no pixels. An empty maxel's size is zero.
    */
-  def isEmpty(implicit E: Eq[A]): Boolean = rep.isEmpty
+  def isEmpty(implicit E: Eq[I], A: Eq[A], M: AdditiveMonoid[A]): Boolean =
+    rep.isEmpty
 
   /**
    * A pixel appears in the union of two maxels a number of times equal to its
    * appearances in one maxel or the other, whichever has more.
    */
-  def union(m: Maxel[A]): Maxel[A] = Maxel(rep union m.rep)
+  def union(m: Maxel[I,A])(implicit L: JoinSemilattice[A],
+                                    M: AdditiveMonoid[A],
+                                    E: Eq[A]): Maxel[I,A] =
+    Maxel(rep union m.rep)
 
   /** An alias for `union`. */
-  def ∪(m: Maxel[A]): Maxel[A] = union(m)
+  def ∪(m: Maxel[I,A])(implicit L: JoinSemilattice[A],
+                                    M: AdditiveMonoid[A],
+                                    E: Eq[A]): Maxel[I,A] = union(m)
 
   /**
    * A pixel appears in the intersection of two maxels a number of times equal
    * to its appearances in one maxel or the other, whichever has fewer.
    */
-  def intersect(m: Maxel[A]): Maxel[A] = Maxel(rep intersect m.rep)
+  def intersect(m: Maxel[I,A])(implicit L: MeetSemilattice[A],
+                                        M: AdditiveMonoid[A],
+                                        E: Eq[A]): Maxel[I,A] =
+    Maxel(rep intersect m.rep)
 
   /** An alias for `intersect`. */
-  def ∩(m: Maxel[A]): Maxel[A] = intersect(m)
+  def ∩(m: Maxel[I,A])(implicit L: MeetSemilattice[A],
+                                M: AdditiveMonoid[A],
+                                E: Eq[A]): Maxel[I,A] = intersect(m)
 
   /**
    * A pixel appears in the sum of two maxels a number of times equal to the
    * sum of its appearances in either maxel.
    */
-  def add(m: Maxel[A]): Maxel[A] = Maxel(rep sum m.rep)
+  def add(m: Maxel[I,A])(implicit A: AdditiveMonoid[A],
+                                  E: Eq[A]): Maxel[I,A] =
+    Maxel(rep sum m.rep)
 
   /** An alias for `add`. */
-  def +(m: Maxel[A]): Maxel[A] = add(m)
+  def +(m: Maxel[I,A])(implicit A: AdditiveMonoid[A],
+                                E: Eq[A]): Maxel[I,A] = add(m)
 
   /**
    * Subtract one maxel from another. Subtracts the appearances of a pixel in
    * the second maxel from its appearances in the first.
    */
-  def subtract(m: Maxel[A]): Maxel[A] = Maxel(rep difference m.rep)
+  def subtract(m: Maxel[I,A])(implicit R: MRealm[A]): Maxel[I,A] =
+    Maxel(rep difference m.rep)
 
   /** Scales a maxel by duplicating all its pixels. */
-  def scale(n: Natural): Maxel[A] = Maxel(rep scale n)
+  def scale(n: A)(implicit M: MultiplicativeSemigroup[A],
+                           N: AdditiveMonoid[A],
+                           E: Eq[A]): Maxel[I,A] =
+    Maxel(rep scale n)
 
   /** The product of two maxels is the product of all their pixels. */
-  def multiply(m: Maxel[A])(implicit E: Eq[A]): Maxel[A] = Maxel(for {
-    p <- rep
-    q <- m.rep
-    r <- MSet.multisetFromSeq((p *? q).toSeq)
-  } yield r)
+  def multiply(m: Maxel[I,A])(implicit E: Eq[I],
+                                       F: Eq[A],
+                                       M: MultiplicativeMonoid[A],
+                                       A: AdditiveMonoid[A]): Maxel[I,A] =
+    Maxel(for {
+      p <- rep
+      q <- m.rep
+      r <- MSet.fromSeq((p *? q).toSeq)
+    } yield r)
 
   /** The product of two maxels is the product of all their pixels. */
-  def *(m: Maxel[A])(implicit E: Eq[A]): Maxel[A] = multiply(m)
+  def *(m: Maxel[I,A])(implicit E: Eq[I],
+                                       F: Eq[A],
+                                       M: MultiplicativeMonoid[A],
+                                       A: AdditiveMonoid[A]): Maxel[I,A] =
+    multiply(m)
 
   /** Get the value at a particular pixel. */
-  def screen(p: Pixel[A]): Natural = rep(p)
+  def screen(p: Pixel[I])(implicit M: AdditiveMonoid[A]): A = rep(p)
 
   /** Get a 2D display of the maxel, starting at pixel 1,1 */
-  def display(implicit I: Integral[A], O: Order[A]): List[List[Natural]] =
+  def display(implicit I: Integral[I],
+                       O: Order[I],
+                       A: AdditiveMonoid[A],
+                       E: Eq[A]): List[List[A]] =
     extent match {
       case Some(Pixel(rows, cols)) =>
         List.range(I.one, I.plus(rows, I.one)).map { row =>
@@ -136,46 +180,60 @@ case class Maxel[A](rep: MSet[Natural, Pixel[A]]) {
     }
 
   /** Get a string representation of the 2D display of the maxel. */
-  def render(implicit I: Integral[A], O: Order[A]): String =
+  def render(implicit I: Integral[I],
+                      O: Order[I],
+                      M: AdditiveMonoid[A],
+                      E: Eq[A]): String =
     display.map(_.mkString("\t")).mkString("\n")
 
   /**
    * Get all the pixels in the cross of another pixel. The cross of a pixel
    * is all pixels in its row and column.
    */
-  def cross(a: Pixel[A])(implicit E: Eq[A]): Maxel[A] =
+  def cross(a: Pixel[I])(implicit E: Eq[I], M: MRealm[A]): Maxel[I,A] =
     Maxel(rep.filter(p => p.col === a.col || p.row === a.row))
 
   /**
    * The cross of a maxel is sets of rows and columns, that is, the set of
    * crosses of its pixels.
    */
-  def cross: Pixel[Set[A]] = rep.foldLeft(Pixel(Set[A](), Set[A]())) {
+  def cross: Pixel[Set[I]] = rep.foldLeft(Pixel(Set[I](), Set[I]())) {
     case (Pixel(rows, cols), (Pixel(row, col), _)) =>
       Pixel(rows + row, cols + col)
   }
 }
 
 object Maxel {
-  def fromSeq[A](s: Seq[Pixel[A]]): Maxel[A] =
-    Maxel(MSet.multisetFromSeq(s))
+  def fromSeq[I,A](s: Seq[Pixel[I]])(
+                   implicit M: MultiplicativeMonoid[A],
+                            A: AdditiveMonoid[A],
+                            E: Eq[A]): Maxel[I,A] =
+    Maxel(MSet.fromSeq(s))
 
-  def singleton[A](p: Pixel[A]): Maxel[A] = fromSeq(Seq(p))
+  def singleton[I,A](p: Pixel[I])(implicit M: MultiplicativeMonoid[A],
+                                           A: AdditiveMonoid[A],
+                                           E: Eq[A]): Maxel[I,A] =
+    fromSeq(Seq(p))
 
-  def e[A](row: A, col: A): Maxel[A] = singleton(Pixel(row, col))
+  def e[I,A](row: I, col: I)(implicit M: MultiplicativeMonoid[A],
+                                      A: AdditiveMonoid[A],
+                                      E: Eq[A]): Maxel[I,A] =
+    singleton(Pixel(row, col))
 
   /** The empty maxel with no pixels. Its size is zero. */
-  def empty[A]: Maxel[A] = Maxel(MSet.empty)
+  def empty[I,A]: Maxel[I,A] = Maxel(MSet.empty)
 
-  implicit def maxelRealm[A:Eq]: Realm[Maxel[A]] = new Realm[Maxel[A]] {
-    def join(a: Maxel[A], b: Maxel[A]) = a ∪ b
-    def meet(a: Maxel[A], b: Maxel[A]) = a ∩ b
-    val zero = empty[A]
-    def plus(a: Maxel[A], b: Maxel[A]) = a + b
-    override def eqv(a: Maxel[A], b: Maxel[A]) = a === b
+  implicit def maxelRealm[I:Eq,A:Realm]: Realm[Maxel[I,A]] = new Realm[Maxel[I,A]] {
+    def join(a: Maxel[I,A], b: Maxel[I,A]) = a ∪ b
+    def meet(a: Maxel[I,A], b: Maxel[I,A]) = a ∩ b
+    val zero = empty[I,A]
+    def plus(a: Maxel[I,A], b: Maxel[I,A]) = a + b
+    override def eqv(a: Maxel[I,A], b: Maxel[I,A]) = a === b
   }
 
-  def partialIdentity[A](s: Set[A]): Maxel[A] =
+  def partialIdentity[I,A](s: Set[I])(implicit M: MultiplicativeMonoid[A],
+                                               A: AdditiveMonoid[A],
+                                               E: Eq[A]): Maxel[I,A] =
     fromSeq(s.toSeq.map(Pixel.diagonal))
 }
 
--- src/test/scala/MaxelTests.scala
+++ src/test/scala/MaxelTests.scala
@@ -7,7 +7,7 @@ import scalaz.std.string._
 
 object MaxelTests extends Scalaprops {
   import maxel._
-  import spire.algebra.Eq
+  import spire.algebra.{Eq, MultiplicativeMonoid, AdditiveMonoid}
   import spire.syntax.all._
   import spire.std.char._
   import spire.std.int._
@@ -17,29 +17,30 @@ object MaxelTests extends Scalaprops {
   import Maxel._
   import PixelTests._
 
-  implicit def genMaxel[A:Gen:Eq]: Gen[Maxel[A]] =
-    Gen[List[Pixel[A]]].map(fromSeq)
+  implicit def genMaxel[A:Gen:Eq:MultiplicativeMonoid:AdditiveMonoid,
+                        I:Gen:Eq]: Gen[Maxel[I,A]] =
+    Gen[List[Pixel[I]]].map(fromSeq[I,A])
 
-  val extentFunctorality = forAll { m: Maxel[Int] =>
+  val extentFunctorality = forAll { m: Maxel[Int,Int] =>
     m.transpose.extent === m.extent.map(_.transpose)
   }
 
-  val intentFunctorality = forAll { m: Maxel[Int] =>
+  val intentFunctorality = forAll { m: Maxel[Int,Int] =>
     m.transpose.intent === m.intent.map(_.transpose)
   }
 
   val constructiveDiagonality = forAll { (m: List[Int]) =>
-    fromSeq(m.map(Pixel.diagonal)).isDiagonal
+    fromSeq[Int,Int](m.map(Pixel.diagonal)).isDiagonal
   }
 
-  val transposeIsomorphism = forAll { (m: Maxel[Int]) =>
+  val transposeIsomorphism = forAll { (m: Maxel[Int,Int]) =>
     m.transpose.transpose === m
   }
 
-  val empty = prop(Maxel.empty[Int].isEmpty)
+  val empty = prop(Maxel.empty[Int,Int].isEmpty)
 
   val nonempty = forAll { (p: List[Pixel[Int]]) =>
-    p.isEmpty == fromSeq(p).isEmpty
+    p.isEmpty == fromSeq[Int,Int](p).isEmpty
   }
 
   def realmLaws[A:Gen:Realm](s: String) = {
@@ -54,25 +55,25 @@ object MaxelTests extends Scalaprops {
       "associativity" -> forAll(associativeLaw[A] _))
   }
 
-  val realm = realmLaws[Maxel[Int]]("maxel realm")
+  val realm = realmLaws[Maxel[Int,Int]]("maxel realm")
 
   val multiplicativeSemigroup =
-    forAll { (a: Maxel[Int], b: Maxel[Int], c: Maxel[Int]) =>
+    forAll { (a: Maxel[Int,Int], b: Maxel[Int,Int], c: Maxel[Int,Int]) =>
       a * (b * c) === (a * b) * c
     }
 
   val multiplicationDistributes =
-    forAll { (a: Maxel[Int], b: Maxel[Int], c: Maxel[Int]) =>
+    forAll { (a: Maxel[Int,Int], b: Maxel[Int,Int], c: Maxel[Int,Int]) =>
       a * (b + c) === ((a * b) + (a * c))
     }
 
-  val multiplicativeIdentity = forAll { (a: Maxel[Int], i: Int) =>
-    (e(i,i) * a === a) === (a.cross.row subsetOf Set(i)) &&
-    (a * e(i,i) === a) === (a.cross.col subsetOf Set(i))
+  val multiplicativeIdentity = forAll { (a: Maxel[Int,Int], i: Int) =>
+    (e[Int,Int](i,i) * a === a) === (a.cross.row subsetOf Set(i)) &&
+    (a * e[Int,Int](i,i) === a) === (a.cross.col subsetOf Set(i))
   }
 
-  val partialIdentities = forAll { (m: Maxel[Int], j: Set[Int]) =>
-    val ej = partialIdentity(j)
+  val partialIdentities = forAll { (m: Maxel[Int,Int], j: Set[Int]) =>
+    val ej = partialIdentity[Int,Int](j)
     ((ej * m).cross.row subsetOf j) && ((m * ej).cross.col subsetOf j)
   }
 }
